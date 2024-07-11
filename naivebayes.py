1d817ed1dfbc84ba68f0af3eace5fe06ab890967
Andrei <andrei.koenich@gmail.com>
2024-07-11 17:51:53 -0300

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -108,7 +108,7 @@ def main():
 
     #print(dados)
     #print(dados_encoded)
-    print(dados_normalizados)
+    #print(dados_normalizados)
 
     instancia_teste = cria_instancia_teste()
     resultado = combinar_naive_bayes(dados_normalizados, instancia_teste)
