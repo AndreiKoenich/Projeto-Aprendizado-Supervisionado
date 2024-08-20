8a94e8701f28eed9b723f763ecc8ded5a7919da4
Felipe Didio <felipe.sdidio@gmail.com>
2024-08-20 20:33:34 -0300

    Renamed etapa2.py to main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+import pprint
+import shutil
+import os
+
 import pandas as pd
 import numpy as np
-import pprint
 
 import utils
 from decision_tree import train_decision_tree
@@ -8,7 +15,6 @@ from knn import train_knn_euc, train_knn_che, train_knn_man
 from naive_bayes import train_naive_bayes_combined
 
 
-# ORCHESTRATION
 def build_models(train_x, train_y, test_x, test_y):
     training_parameters = [
         { 'label': 'decision_tree',         'training_function': train_decision_tree,           'input_function': utils.identity            },
@@ -20,7 +26,7 @@ def build_models(train_x, train_y, test_x, test_y):
 
     models = []
     
-    print('\nIniciando treinamento dos modelos...\n')
+    print('Iniciando treinamento dos modelos...\n')
     for param in training_parameters:
         label = param['label']
         training_function = param['training_function']
@@ -34,39 +40,102 @@ def build_models(train_x, train_y, test_x, test_y):
     return models
 
 
-def main():
-    # Import and scramble data
-    data = utils.import_data()
+def run_test(data, root_dir, *, 
+             remove_outliers=True,
+             train_data_strategy='holdout'
+             ):
+    # Data pre-processing
+    if remove_outliers:
+        data = utils.remove_outliers(data)
+        data.to_csv('outlier_free_data.csv', index=False)
+    data.to_csv('raw_data.csv', index=False)
+    data = utils.one_hot_encoding(data)
+    data = utils.normalizar_dados(data)
     data = utils.scramble_data(data)
-    # data.to_csv('all_data.csv', index=False)
-    # data.to_csv('all_data.csv', index=False)
+    data.to_csv('clean_data.csv', index=False)
 
-    # Split into train and test data
-    train_data, val_data, test_data = utils.stratified_split(data)
+    # Data splitting
+    if train_data_strategy == 'holdout':
+        train_data, val_data, test_data = utils.stratified_split(data)
+        train_x, train_y = utils.xy_split(train_data, columns=['Weather Type'])
+        test_x, test_y = utils.xy_split(test_data, columns=['Weather Type'])
+        val_x, val_y = utils.xy_split(val_data, columns=['Weather Type'])
+    elif train_data_strategy == 'bootstrap':
+        train_data, val_data, test_data = utils.bootstrap(data)
+        train_x, train_y = utils.xy_split(train_data, columns=['Weather Type'])
+        test_x, test_y = utils.xy_split(test_data, columns=['Weather Type'])
+        val_x, val_y = utils.xy_split(val_data, columns=['Weather Type'])
+    else:
+        raise ValueError(f'Cannot run test with parameter train_data_strategy set to "{train_data_strategy}"')
 
-    # Split into x and y columns
-    train_x, train_y = utils.xy_split(train_data, columns=['Weather Type'])
-    test_x, test_y = utils.xy_split(test_data, columns=['Weather Type'])
-    val_x, val_y = utils.xy_split(val_data, columns=['Weather Type'])
-
+    # Model building
     models = build_models(train_x, train_y, test_x, test_y)
 
-    print('ACURACIAS OBTIDAS COM OTIMIZACAO DE HIPERPARAMETROS COM O CONJUNTO DE VALIDACAO:\n')
+    # Model evaluation
+    with open(f'{root_dir}/test_data.txt', 'a') as file:
+        for model in models:
+            model_label = model['label']
+            model_obj = model['model']
+            accuracy = model_obj.score(val_x, val_y)
+            model['test_accuracy'] = accuracy
+
+            file.write(f'[Test "{model_label}"]\n')
+            file.write(f'accurarcy={accuracy}\n')
+            file.write(f'model={model_obj.__repr__()}\n')
+            file.write(f'\n')
+
+    # ROC e PR
     for model in models:
         model_label = model['label']
         model_obj = model['model']
-        accuracy = model_obj.score(val_x, val_y)
-        print(f'Modelo "{model_label}":\n{accuracy}')
+        model_dir = f'{root_dir}/{model_label}'
+        os.makedirs(model_dir, exist_ok=True)
+        utils.makeRocCurve(model_obj, f'train_{model_label}', test_x, test_y, train_y, model_dir)
+        utils.makePrCurve(model_obj, f'train_{model_label}', test_x, test_y, train_y, model_dir)
+    
+    return models
 
-    #pprint.pprint(models)
 
-    print('\nACURACIAS OBTIDAS APOS OTIMIZACAO DE HIPERPARAMETROS COM O CONJUNTO DE TESTE:\n')
-    print('Acuracia do modelo Arvore de Decisao:\n', models[0]['accuracy'])
-    print('Acuracia do modelo kNN com distancia Euclidiana:\n', models[1]['accuracy'],' k = ',models[1]['model'].n_neighbors)
-    print('Acuracia do modelo kNN com distancia Chebyshev:\n', models[2]['accuracy'],' k = ',models[2]['model'].n_neighbors)
-    print('Acuracia do modelo kNN com distancia Manhattan:\n', models[3]['accuracy'],' k = ',models[3]['model'].n_neighbors)
-    print('Acuracia do modelo Naive Bayes:\n', models[4]['accuracy'],'\n')
+def main():
+    # Import data
+    filename = 'weather_classification_data.csv'
+    data = utils.ler_dados_csv(filename)
+
+    result_set = []
+
+    strategies = [ 'holdout', 'bootstrap' ]
+
+    test_index = 0
+    #shutil.rmtree('tests')
+    for remove_outliers in range(2):
+        for test_strategy in strategies:
+            # Test environment setup
+            test_index += 1
+            test_directory = f'tests/test_{test_index}'
+            os.makedirs(test_directory, exist_ok=True)
+            
+            # Write test's description.txt
+            with open(f'{test_directory}/test_data.txt', 'w') as file:
+                file.write(f'[parameters]\n')
+                file.write(f'remove_outliers={bool(remove_outliers)}\n')
+                file.write(f'test_strategy={test_strategy}\n')
+                file.write(f'\n')
+
+            # Run test (store data in test_1)
+            models = run_test(data, test_directory,
+                              remove_outliers=bool(remove_outliers),
+                              train_data_strategy=test_strategy,
+                              )
+
+            # Store data in data structure
+            result_set.append({
+                'test_index': test_index,
+                'remove_outliers': bool(remove_outliers),
+                'test_strategy': test_strategy,
+                'models': models,
+                })
 
 
 if __name__ == '__main__':
     main()
+
